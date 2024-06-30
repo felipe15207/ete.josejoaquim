bdd0a8885ec78907a92c1bd0db22b51dd63d1bac
Pedro Felipe <167935817+felipe15207@users.noreply.github.com>
2024-06-30 02:35:46 -0300

    Ajuste no menu para mobile e animação do botão de abrir menu

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+
+/* FUNÇÃO RESPONSÁVEL POR ABRIR O MENU */
+
+$(document).ready(function(){
+    $('#btn-menu').click(function(){
+        $('#btn-menu').toggleClass('ativado');
+    });
+  });
+
+/* FUNÇÃO RESPONSÁVEL POR ABRIR O MENU */
